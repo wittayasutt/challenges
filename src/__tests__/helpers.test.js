c0b7a0544d3034e75a74ae68cd4da37c8e2d7d8c
wittayasutt <wittayasutt@gmail.com>
2018-08-10 04:47:31 +0700

    add toCurrency test

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -1,7 +1,11 @@
-import { summaryDonations } from '../helpers'
+import { summaryDonations, toCurrency } from '../helpers'
 
 describe('helpers', function() {
 	test('`summaryDonations` should calculate donations correctly', function() {
-		expect(summaryDonations([1, 2, 3, 4])).toEqual(11)
+		expect(summaryDonations([1, 2, 3, 4])).toEqual(10)
+	})
+
+	test('`toCurrency` should change number to currency', function() {
+		expect(toCurrency(1234567)).toEqual('1,234,567')
 	})
 })
